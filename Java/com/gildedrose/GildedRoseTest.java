1ef6161cfa779456d671eca6af4d61b7db06861f
Jose Enrique Rodriguez Huerta <staff.tsr@gmail.com>
2015-10-27 11:07:10 +0100

    Adding test to check normal items decrease quality at least by one

--- Java/com/gildedrose/GildedRoseTest.java
+++ Java/com/gildedrose/GildedRoseTest.java
@@ -27,7 +27,15 @@ public class GildedRoseTest {
     	Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 1, 80) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals(80, app.items[0].quality);
+        assertEquals(80, app.items[0].quality); 
+	}
+    
+    @Test
+	public void IfItemIsNormalQualityDecreasesByAtLeastOne() throws Exception {
+    	Item[] items = new Item[] { new Item("Normal", 0, 1) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertTrue(app.items[0].quality < 1);
 	}
 
 }
